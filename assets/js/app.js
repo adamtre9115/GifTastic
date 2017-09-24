fcecc08b8e2499a519816b1ba10336a2d5e71493
adamtre9115 <treadam9115@gmail.com>
2017-09-24 17:59:32 -0400

    add new catagory on submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,34 @@
 // api key 6ZmJCr6fkcI6grbwXy443FPGFOIlRPF9
 var gifs = ["hello", "swerve", "lit", "savage", "oh-snap", "lol"];
 
+
+
 function buttonDisplay(){
     for (var i = 0; i < gifs.length; i++){
-       var $newButton = $("<button></button>")
+       var $newButton = $("<button></button>");
        $($newButton).attr("id", gifs[i]);
         $($newButton).html(gifs[i]);
         $("#buttons").append($newButton);
     }
 }
 buttonDisplay();
+
+// append new button on submit button click
+function addCat(){
+    $("#newCat").on("click",function(){
+       var newCategory = $("#newGif").val();
+       console.log(newCategory);
+       var $newButton = $("<button></button>");
+       $($newButton).attr("id", newCategory);
+       $($newButton).html(newCategory);
+       $("#buttons").append($newButton);
+       clearField();
+       event.preventDefault();
+    })
+}
+addCat()
+
+// Clear input field
+function clearField(){
+    $("#newGif").val("");
+}
